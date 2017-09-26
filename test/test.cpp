442b07eef4043065de119c41a5f9c36a5a9796c3
Yuyu Hsueh <yhsueh@terpmail.umd.edu>
2017-09-26 12:29:50 -0400

    update First test

--- test/test.cpp
+++ test/test.cpp
@@ -3,9 +3,12 @@
 #include <memory>
 
 struct ControllerTest : testing :: Test{
-  auto subject = std::make_shared<controller> (5,1);
+  std::shared_ptr<Controller> subject = std::make_shared<Controller> (5,1);
 };
+
+
 TEST_F(ControllerTest, should_pass) {
 
   EXPECT_LT(1, 999);
 }
+
