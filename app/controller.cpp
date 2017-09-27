7dec90055657ba3ff491cbcd7e259d08f7e51f9f
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-09-26 19:33:06 -0700

    Implement the PID control method in the comput_vel function

--- app/controller.cpp
+++ app/controller.cpp
@@ -12,24 +12,24 @@
 #include "controller.hpp"
 
 /**
-* @brief Compute current error
-* @param None
-* @return error
-*/
+ * @brief Compute current error
+ * @param None
+ * @return error
+ */
 double Controller::compute_error() {
-  double error;
-  error = 1000;
+  double error = des_vel - cur_vel;
   return error;
 }
 
-
 /**
-* @brief Compute new velocity using PID equation
-* @param None
-* @return new velocity
-*/
+ * @brief Compute new velocity using PID equation
+ * @param None
+ * @return new velocity
+ */
 double Controller::compute_vel() {
-  double new_vel = 1000;
+  double new_vel = kp * (this->compute_error())
+      + kd * (this->compute_error() - pre_err)
+      + ki * (sum_err + this->compute_error());
   return new_vel;
 }
 
